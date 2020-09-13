770bbad47b661cba8fa67d7e21bb65523bd9e5ec
Jan Bölsche <regular.gonzales@googlemail.com>
2020-09-13 18:37:16 +0200

    Add clock and minute hand

--- index.js
+++ index.js
@@ -1,38 +1,58 @@
 const s = require('mutant/svg-element')
+const h = require('mutant/html-element')
 const computed = require('mutant/computed')
 const Value = require('mutant/value')
 const spiral = require('./spiral')
+const styles = require('module-styles')('time-spiral')
 
-const t = Value(90)
+const t = Value(0)
 
 setInterval(()=>{
   t.set(t()+1)
-}, 10)
+}, 1000)
 
 document.body.appendChild(
-  s('svg', {
-    width: 400,
-    height: 400,
-    viewBox: "0 0 800 800"
-  }, [
-    s('g', {
-      transform: computed(t, t=> `translate(400,400) scale(-1 1) rotate(-${t})`)
+  h('.time-spiral', [
+    h('.clock', computed(t, t=>{
+      return `${Math.floor(t/60)}:${t%60}`
+    })),
+    s('svg', {
+      width: 400,
+      height: 400,
+      viewBox: "-400 -400 800 800"
     }, [
-      s('path', {
-        d: spiral({
-          x:0,
-          y:0,
-          startRadius: 300,
-          spacePerLoop: -30,
-          startTheta:rad(-90),
-          endTheta: rad(10*360),
-          thetaStep: rad(30)
-        }),
-        fill: 'none',
-        stroke: '#aaa',
-        'stroke-width': 20,
-        'stroke-linecap': 'round'
-      })
+      s('g', {
+        
+      }, [
+        s('g.spiral', {
+          transform: computed(t, t=> `scale(-1 1) rotate(-${t * 6})`)
+        }, [
+          s('path', {
+            d: spiral({
+              x:0,
+              y:0,
+              startRadius: 300,
+              spacePerLoop: -30,
+              startTheta:rad(-90),
+              endTheta: rad(10*360),
+              thetaStep: rad(30)
+            }),
+            fill: 'none',
+            stroke: '#aaa',
+            'stroke-width': 20,
+            'stroke-linecap': 'round'
+          })
+        ]),
+        s('line.minute-hand', {
+          x1: 0, 
+          y1: 0,
+          x2: 0,
+          y2: -400,
+          'stroke-width': 10,
+          stroke: 'black',
+          transform: computed(t, t=> `rotate(${t * 6})`)
+        })
+      ])
     ])
   ])
 )
@@ -40,3 +60,11 @@ document.body.appendChild(
 function rad(x) {
   return x * Math.PI / 180
 }
+
+styles(`
+.clock {
+  font-family: monospace;
+  font-size: 56pt;
+  color: #aaa;
+}
+`)
